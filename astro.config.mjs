1d4d542963df0b06e55cf27701f67ea6b0aa9dee
Luke.Batchelor <batchelor.luke+git@gmail.com>
2023-01-11 16:58:21 +0800

    Adds a basic index page and some placeholder content for projects, blob and contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind(), mdx()]
+});
